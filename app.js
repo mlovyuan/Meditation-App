64382f0276244be482f9210dae873cd61ec652ff
mlovyuan <mlovyuan@yahoo.com.tw>
2021-11-30 23:39:37 +0800

    Almost finish, timediplay is werong

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const app = () => {
   const video = document.querySelector(".vid- container video");
   const sounds = document.querySelectorAll(".sound-picker button");
-  const timeDisplay = document.querySelector(".time-display");
   const outline = document.querySelector(".moving-outline circle");
   const outlineLength = outline.getTotalLength();
-
-  let fakeDuration = 600;
 
   outline.style.strokeDasharray = outlineLength;
   outline.style.strokeDashoffset = outlineLength;
@@ -26,6 +23,29 @@ const app = () => {
       video.pause();
       play.src = "./svg/play.svg";
     }
+  };
+
+  let fakeDuration = 600;
+  const timeSelect = document.querySelectorAll(".time select button");
+  timeSelect.forEach((timeButton) => {
+    timeButton.addEventListener("click", () => {
+      fakeDuration = this.getAttribute("data-time");
+      timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+        fakeDuration % 60
+      )}`;
+    });
+  });
+
+  const timeDisplay = document.querySelector(".time-display");
+  song.ontimeupdate = () => {
+    let currentTime = song.currentTime;
+    let elapsed = fakeDuration - currentTime;
+    let seconds = Math.floor(elapsed % 60);
+    let minutes = Math.floor(elapsed / 60);
+
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
+    timeDisplay.textContent = `${minutes}:${seconds}`;
   };
 };
 
